bedf36e832cf129943d665e2065b9b0e30caf03a
yxleong <yx0829leong@gmail.com>
2023-05-10 00:51:04 +0800

    Add files via upload

---
+++ OOP/DungeonPractice01_BasicIO/README.md
@@ -0,0 +1,12 @@
+# Basic IO
+This program continuously reads user input in a loop and uses a switch statement to judge the input.
+
+## Function Implementation
+The program implements the following features:
+* The program continuously reads user input in a loop.
+* The program uses a switch statement to judge the input.
+* When `W`, `A`, `S`, or `D` is pressed, the program outputs the corresponding message.
+* Pressing the `Esc` key ends the program and outputs the end message.
+
+## How to Play
+To use the program, run it and press `W`, `A`, `S`, or `D` to see the corresponding message outputted. Press the `Esc` key to end the program and see the end message.
---
+++ OOP/DungeonPractice01_BasicIO/main.cpp
@@ -0,0 +1,125 @@
+﻿/*****************************************************************//**
+ * File: main.cpp
+ * Author:
+ *	Leong Yah Xuan
+ *	B11015042
+ * Create Date: February 21, 2023
+ * Update Date: February 21, 2023
+ * Description: This C++ program implements basic user input control.
+ *********************************************************************/
+
+ /************************************************************************
+ File:   Source.cpp
+
+ Author:
+		 Áé½å¼s¡Aea5878158@gmail.com
+ Modifier:
+		 ¿à¯§¦N¡Acheeryuchi@gmail.com
+		 ³¯«T¦t¡AJYCReports@gmail.com
+ Comment:
+		 Output W,A,S,D after input, ESC to stop process.
+
+ ************************************************************************/
+#include <iostream>	// For print out information
+#include <conio.h>	// For getch() function
+
+ // Define inputs and its index
+enum ValidInput
+{
+	EW = 0,
+	ES = 1,
+	EA = 2,
+	ED = 3,
+	ESPACE = 4,
+	EESC = 5,
+	INVALID,
+};
+
+// Detect state of input
+void keyUpdate(bool key[]);
+
+int main()
+{
+	//Initialize state of key
+	bool gKeyState[ValidInput::INVALID];
+
+	// Obtain and update the input using a loop
+	/************************************************************************/
+	/*Please implement your code here*/
+
+	while (true) {
+		// continuously read user input in a loop
+
+		keyUpdate(gKeyState);
+
+		// End the expression and output the end message when pressing the Esc key
+		if (gKeyState[ValidInput::EESC]) {
+			std::cout << "Key ESC is pressed. End program." << std::endl;
+			break;
+		}
+		else {
+			if (gKeyState[ValidInput::EW])
+				std::cout << "Key W is pressed." << std::endl;
+			else if (gKeyState[ValidInput::ES])
+				std::cout << "Key S is pressed." << std::endl;
+			else if (gKeyState[ValidInput::EA])
+				std::cout << "Key A is pressed." << std::endl;
+			else if (gKeyState[ValidInput::ED])
+				std::cout << "Key D is pressed." << std::endl;
+			else
+				std::cerr << "Wrong input!" << std::endl;
+		}
+	}
+
+	return 0;
+
+	/************************************************************************/
+}
+
+//******************************************************************
+//
+// * °»´ú¿é¤Jª¬ºA
+//==================================================================
+void keyUpdate(bool key[])
+//==================================================================
+{
+	for (int i = 0; i < ValidInput::INVALID; i++) {
+		key[i] = false;
+	}
+	char input = _getch();
+
+	// Implement a switch for output calls
+	/************************************************************************/
+	/*Please implement your code here*/
+
+	switch (input) {
+		// Use the switch to judge the input
+		// when pressing W, A, S, D to input the corresponding message
+	case 'w':
+	case 'W':
+		key[ValidInput::EW] = true;
+		break;
+	case 's':
+	case 'S':
+		key[ValidInput::ES] = true;
+		break;
+	case 'a':
+	case 'A':
+		key[ValidInput::EA] = true;
+		break;
+	case 'd':
+	case 'D':
+		key[ValidInput::ED] = true;
+		break;
+	case ' ':
+		key[ValidInput::ESPACE] = true;
+		break;
+	case 27:
+		key[ValidInput::EESC] = true;
+		break;
+	default:
+		break;
+	}
+
+	/************************************************************************/
+}
