ff8c70528e3cf747461e8c3a85cf21ba6eead294
yxleong <yx0829leong@gmail.com>
2023-06-23 18:44:34 +0800

    Add files via upload

---
+++ OOP/Coursework1501_SetExercise/README.md
@@ -0,0 +1,39 @@
+# Set Exercise
+
+## Problem Statement
+Consider a text file of names with one name per line, and the names have been collected from several different sources.
+
+A sample is shown in the following: 
+
+```
+Brooke Trout 
+Dinah Soars 
+Jed Dye 
+Brooke Trout 
+Jed Dye 
+Paige Turner 
+```
+
+There are duplicate names in the file. We would like to generate an invitation list but do not want to send multiple invitations to the same person.
+
+Write a program that eliminates the duplicate names by using the set template class. Read each name from the file (`name.txt`) and add it to the set, then output all names in the set to generate the invitation list without duplicates. 
+
+
+## Input
+Read the file named "name.txt".
+```
+name.txt
+```
+
+## Output
+```
+Brooke Trout
+Dinah Soars
+Jed Dye
+Paige Turner
+```
+
+## Additional Information
+* Easy, only basic programming syntax and structure are required.
+* Solving time: 10 minutes
+* Contributor: 謝宜杭, 謝公耀, 廖宣瑋
---
+++ OOP/Coursework1501_SetExercise/main.cpp
@@ -0,0 +1,45 @@
+﻿/*****************************************************************//**
+ * File: main.cpp
+ * Author: Leong Yah Xuan
+ * Create Date: June 6, 2023
+ * Update Date: June 9, 2023
+ * Description: This C++ program eliminates the duplicate names by using the set template class.
+ *********************************************************************/
+#include <iostream>
+#include <set>
+#include <fstream>
+#include <string>
+
+int main(void) {
+
+	// open file
+	std::ifstream inFile("name.txt");
+
+	// check if file is correctly opened
+	if (!inFile.is_open()) {
+		// if not, stop system
+		return 0;
+	}
+
+	// declare the variables needed
+	std::set<std::string> nameList;
+	std::string names = "";
+
+	// read file content line by line
+	while (std::getline(inFile, names)) {
+		// insert content in line to set
+		nameList.insert(names);
+		// without duplicate content
+	}
+
+	// close the file
+	inFile.close();
+
+	// loop the content in set
+	for (auto& name : nameList) {
+		// output the content in set
+		std::cout << name << std::endl;
+	}
+
+	return 0;
+}
