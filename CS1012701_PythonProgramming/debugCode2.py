f030923c7478ece6bd75daa0919a2bda5a1d1a07
yxleong <yx0829leong@gmail.com>
2023-09-14 16:22:28 +0800

    Create debugCode2.py

---
+++ CS1012701_PythonProgramming/debugCode2.py
@@ -0,0 +1,7 @@
+def sum_2_numbers(a, b):
+    return a + b
+    
+for i in range(5):
+    for j in range(4):
+        # print(sum_2_numbers(i, j))
+        print("the sum of", i, "and", j, "is", sum_2_numbers(i, j))
