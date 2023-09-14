0cde8e372b604388c9b74caafb761a7f3f7332ef
yxleong <yx0829leong@gmail.com>
2023-09-14 16:09:23 +0800

    Add files via upload

---
+++ CS1012701_PythonProgramming/debugCode3.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.DataFrame(data={"id": ['a', 'b', 'c'],
+                        "value": [1, 2, 3]})
+                        
+print(df)
+
+def multiply_value(df, multiplier):
+    df = df.copy()
+    df["value"] = df["value"] * multiplier
+    
+#multiplier_list = [1, 2, "3"]
+multiplier_list = [1, 2, 3]
+
+for mult in multiplier_list:
+    multiply_value(df, mult)
