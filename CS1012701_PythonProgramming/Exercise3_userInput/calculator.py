fbd55f4d1c07eef1b41017c80310339f38f59eed
yxleong <yx0829leong@gmail.com>
2023-10-01 22:23:01 +0800

    add files

---
+++ CS1012701_PythonProgramming/Exercise3_userInput/calculator.py
@@ -0,0 +1,11 @@
+num1 = float(input("Input first number: "))
+num2 = float(input("Input second number: "))
+
+print("Sum:", num1 + num2)
+print("Sub:", num1 - num2)
+print("Mul:", num1 * num2)
+
+if(num2 != 0):
+   print("Div:",num1 / num2)
+else:
+   print("Div: ERROR! Division by Zero")
---
+++ CS1012701_PythonProgramming/Exercise3_userInput/temperature.py
@@ -0,0 +1,5 @@
+celcius = float(input("Input celcius: "))
+
+fahrenheit = celcius * 9 / 5 + 32
+
+print(celcius, "°C  is ", fahrenheit, "°F\n")
