95fbe3db3f3f650aac492b38064ef0ba2d66a6ca
yxleong <yx0829leong@gmail.com>
2023-10-13 20:44:44 +0800

    Update tuple.py
    Q1 TA solution

--- CS1012701_PythonProgramming/Exercise5-1_tuple/tuple.py
+++ CS1012701_PythonProgramming/Exercise5-1_tuple/tuple.py
@@ -13,6 +13,14 @@ for i in range(size):
         sum += back
     result += (sum, )
 print(result)
+# ---------------------------------------------------
+t = (1, 5, 7, 8, 10)
+l = []
+l.append(t[0]*t[1])
+for i in range(1,len(t)-1):
+    l.append(t[i]*t[i-1]+t[i]*t[i+1])
+l.append(t[-1]*t[-2])
+print(l)
 
 # Q2
 t1 = (1, 2, 3, 0)
