313f502957ae564f4f1da123aa260d0bbe4b0034
yxleong <yx0829leong@gmail.com>
2024-09-13 16:36:29 +0800

    CH-3

---
+++ CS2005701_WindowsProgramming/CH02-3_Swap/Swap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("before swapping: a = " + a + ", b = " + b);
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("after swapping: a = " + a + ", b = " + b);
+        }
+    }
+}
