1ba01e89bc8b2570ff3b03fbfac59a7df3595276
yxleong <yx0829leong@gmail.com>
2024-09-13 15:30:42 +0800

    Update DataTypes.cs

--- CS2005701_WindowsProgramming/CH02-1_DataTypes/DataTypes.cs
+++ CS2005701_WindowsProgramming/CH02-1_DataTypes/DataTypes.cs
@@ -15,8 +15,8 @@ namespace DataTypes
             Console.WriteLine("Boolean: {0} and {1}", true, false);
 
             Console.WriteLine("Char: {0}, {1}, and {2}", 'A', '\u0041', '\u0042');  // Unicode Character
-            Console.WriteLine("65 is " + (char) 65);
-            Console.WriteLine("A is " + (int) 'A');
+            Console.WriteLine("65 is " + (char)65);
+            Console.WriteLine("A is " + (int)'A');
 
             Console.WriteLine("String: {0}, {1}", "Hello", "C#");
         }
