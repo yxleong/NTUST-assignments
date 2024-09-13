147999301c5027fecb739986f37fe1c2fccce08b
yxleong <yx0829leong@gmail.com>
2024-09-13 10:26:35 +0800

    Update Calculation.cs

--- CS2005701_WindowsProgramming/CH01-2_Calculation/Calculation.cs
+++ CS2005701_WindowsProgramming/CH01-2_Calculation/Calculation.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Calculation
 {
-    class Calculation
+    class Program
     {
         static void Main(string[] args)
         {
