c40f9bb1ffcd4a4773ef43efe3a09b9bbac547b8
yxleong <yx0829leong@gmail.com>
2024-09-13 09:50:16 +0800

    first exercise uploaded

---
+++ CS2005701_WindowsProgramming/ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("My first C# program!");
+            Console.WriteLine("no need type System also can");
+            Console.Write("Please input any string: ");
+
+            string input;
+            input = Console.ReadLine();
+            Console.WriteLine("Your input is: " + input);
+        }
+    }
+}
