749c22731b55f8b296ad7e2b60a29d2d3ef08e73
yxleong <yx0829leong@gmail.com>
2024-09-20 10:57:54 +0800

    exercise1 uploaded

---
+++ CS2005701_WindowsProgramming/Eexercise1_ButtonSimpleAdder/SimpleAdderForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace SimpleAdder
+{
+    partial class SimpleAdderForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 19F);
+            this.button1.Location = new System.Drawing.Point(69, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 122);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Press Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 19F);
+            this.label1.Location = new System.Drawing.Point(116, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This is label";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox1.Location = new System.Drawing.Point(142, 269);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(73, 58);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "This is default text...";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 19F);
+            this.button2.Location = new System.Drawing.Point(599, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox2.Location = new System.Drawing.Point(420, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 27);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Enter += this.textBox_Enter;
+            this.textBox2.Leave += this.textBox_Leave;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.textBox3.Location = new System.Drawing.Point(420, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(173, 27);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Enter += this.textBox_Enter;
+            this.textBox3.Leave += this.textBox_Leave;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 19F);
+            this.label2.Location = new System.Drawing.Point(674, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Total";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CS2005701_WindowsProgramming/Eexercise1_ButtonSimpleAdder/SimpleAdderForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;   //  Color
+
+namespace SimpleAdder
+{
+    public partial class SimpleAdderForm : Form
+    {
+        public SimpleAdderForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button Clicked!");
+        }
+        
+        // ------------------------------------------------------------------
+        private void SetDefaultPrompt(TextBox textbox, string prompt)
+        {
+            textbox.Text = prompt;
+            textbox.ForeColor = Color.Gray;
+        }
+
+        private void ClearTextBox(TextBox textBox, string prompt)
+        {
+            textBox.Clear();
+            SetDefaultPrompt(textBox, prompt);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetDefaultPrompt(textBox2, "Input the first number");
+            SetDefaultPrompt(textBox3, "Input the second number");
+        }
+
+        private void textBox_Enter(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if(textBox != null && textBox.ForeColor == Color.Gray)
+            {
+                textBox.Clear();
+                textBox.ForeColor = Color.Black;
+                label2.Text = "Sum: ";
+            }
+        }
+
+        private void textBox_Leave(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                SetDefaultPrompt(textBox, textBox == textBox2 ? "Input the first number" : "Input the second number");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if(double.TryParse(textBox2.Text, out num1) && double.TryParse(textBox3.Text, out num2))
+            {
+                double sum = num1 + num2;
+                label2.Text = "Sum: " + sum.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please input a valid number!");
+            }
+
+            ClearTextBox(textBox2, "Input the first number");
+            ClearTextBox(textBox3, "Input the second number");
+        }
+    }
+}
