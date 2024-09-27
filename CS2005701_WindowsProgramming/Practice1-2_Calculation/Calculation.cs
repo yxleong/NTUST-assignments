36bc51f3880e0351d25f31eb21268f0d7a98b0c5
yxleong <yx0829leong@gmail.com>
2024-09-27 10:33:37 +0800

    Update Calculation.cs

--- CS2005701_WindowsProgramming/Practice1-2_Calculation/Calculation.cs
+++ CS2005701_WindowsProgramming/Practice1-2_Calculation/Calculation.cs
@@ -38,31 +38,3 @@ namespace Calculation
         }
     }
 }
-
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataTypes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Integer: " + 100);
-            Console.WriteLine("Float: " + 3.14159);
-            Console.WriteLine("Boolean: {0} and {1}" + true, false);
-
-            Console.WriteLine("Char: {0}, {1}, and {2}", 'A', '\u0041', '\u0042');
-            Console.WriteLine("65 is: ", + (char)65);
-            Console.WriteLine("A is: ", + (int)'A');
-
-            Console.WriteLine("String: {0}, {1}", "Hello", "C#");
-
-        }
-    }
-}
-
