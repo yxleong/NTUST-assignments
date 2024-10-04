903222b672aa943a8475ab822e254a0629db7598
yxleong <yx0829leong@gmail.com>
2024-10-04 15:45:05 +0800

    upload hw1

---
+++ CS2005701_WindowsProgramming/Exercise1_POS/Form1.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace POSOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkBPizza = new System.Windows.Forms.CheckBox();
+            this.chkSPizza = new System.Windows.Forms.CheckBox();
+            this.chkChicken = new System.Windows.Forms.CheckBox();
+            this.chkFish = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rdbSmall = new System.Windows.Forms.RadioButton();
+            this.chkFries = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.rdbCoffee = new System.Windows.Forms.RadioButton();
+            this.rdbCoke = new System.Windows.Forms.RadioButton();
+            this.chkDrink = new System.Windows.Forms.CheckBox();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.chkBPizza);
+            this.groupBox1.Controls.Add(this.chkSPizza);
+            this.groupBox1.Controls.Add(this.chkChicken);
+            this.groupBox1.Controls.Add(this.chkFish);
+            this.groupBox1.Location = new System.Drawing.Point(119, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 221);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "漢堡/披薩";
+            // 
+            // chkBPizza
+            // 
+            this.chkBPizza.AutoSize = true;
+            this.chkBPizza.Location = new System.Drawing.Point(20, 187);
+            this.chkBPizza.Name = "chkBPizza";
+            this.chkBPizza.Size = new System.Drawing.Size(132, 24);
+            this.chkBPizza.TabIndex = 3;
+            this.chkBPizza.Text = "大披薩 $429";
+            this.chkBPizza.UseVisualStyleBackColor = true;
+            // 
+            // chkSPizza
+            // 
+            this.chkSPizza.AutoSize = true;
+            this.chkSPizza.Location = new System.Drawing.Point(20, 139);
+            this.chkSPizza.Name = "chkSPizza";
+            this.chkSPizza.Size = new System.Drawing.Size(132, 24);
+            this.chkSPizza.TabIndex = 2;
+            this.chkSPizza.Text = "小披薩 $259";
+            this.chkSPizza.UseVisualStyleBackColor = true;
+            // 
+            // chkChicken
+            // 
+            this.chkChicken.AutoSize = true;
+            this.chkChicken.Location = new System.Drawing.Point(20, 94);
+            this.chkChicken.Name = "chkChicken";
+            this.chkChicken.Size = new System.Drawing.Size(163, 24);
+            this.chkChicken.TabIndex = 1;
+            this.chkChicken.Text = "勁辣雞腿堡 $69";
+            this.chkChicken.UseVisualStyleBackColor = true;
+            // 
+            // chkFish
+            // 
+            this.chkFish.AutoSize = true;
+            this.chkFish.Location = new System.Drawing.Point(20, 47);
+            this.chkFish.Name = "chkFish";
+            this.chkFish.Size = new System.Drawing.Size(143, 24);
+            this.chkFish.TabIndex = 0;
+            this.chkFish.Text = "麥香魚堡 $59";
+            this.chkFish.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.rdbSmall);
+            this.groupBox2.Controls.Add(this.chkFries);
+            this.groupBox2.Location = new System.Drawing.Point(451, 45);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(234, 102);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 72);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(102, 24);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "大薯 $35";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // rdbSmall
+            // 
+            this.rdbSmall.AutoSize = true;
+            this.rdbSmall.Location = new System.Drawing.Point(7, 38);
+            this.rdbSmall.Name = "rdbSmall";
+            this.rdbSmall.Size = new System.Drawing.Size(102, 24);
+            this.rdbSmall.TabIndex = 1;
+            this.rdbSmall.TabStop = true;
+            this.rdbSmall.Text = "小薯 $25";
+            this.rdbSmall.UseVisualStyleBackColor = true;
+            // 
+            // chkFries
+            // 
+            this.chkFries.AutoSize = true;
+            this.chkFries.Location = new System.Drawing.Point(0, 14);
+            this.chkFries.Name = "chkFries";
+            this.chkFries.Size = new System.Drawing.Size(71, 24);
+            this.chkFries.TabIndex = 0;
+            this.chkFries.Text = "薯條";
+            this.chkFries.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.textBox9);
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Controls.Add(this.rdbCoffee);
+            this.groupBox3.Controls.Add(this.rdbCoke);
+            this.groupBox3.Controls.Add(this.chkDrink);
+            this.groupBox3.Location = new System.Drawing.Point(451, 146);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(234, 133);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(7, 110);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(102, 24);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "紅茶 $20";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // rdbCoffee
+            // 
+            this.rdbCoffee.AutoSize = true;
+            this.rdbCoffee.Location = new System.Drawing.Point(7, 76);
+            this.rdbCoffee.Name = "rdbCoffee";
+            this.rdbCoffee.Size = new System.Drawing.Size(102, 24);
+            this.rdbCoffee.TabIndex = 4;
+            this.rdbCoffee.TabStop = true;
+            this.rdbCoffee.Text = "咖啡 $30";
+            this.rdbCoffee.UseVisualStyleBackColor = true;
+            // 
+            // rdbCoke
+            // 
+            this.rdbCoke.AutoSize = true;
+            this.rdbCoke.Location = new System.Drawing.Point(7, 42);
+            this.rdbCoke.Name = "rdbCoke";
+            this.rdbCoke.Size = new System.Drawing.Size(102, 24);
+            this.rdbCoke.TabIndex = 3;
+            this.rdbCoke.TabStop = true;
+            this.rdbCoke.Text = "可樂 $25";
+            this.rdbCoke.UseVisualStyleBackColor = true;
+            // 
+            // chkDrink
+            // 
+            this.chkDrink.AutoSize = true;
+            this.chkDrink.Location = new System.Drawing.Point(0, 11);
+            this.chkDrink.Name = "chkDrink";
+            this.chkDrink.Size = new System.Drawing.Size(71, 24);
+            this.chkDrink.TabIndex = 0;
+            this.chkDrink.Text = "飲料";
+            this.chkDrink.UseVisualStyleBackColor = true;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(139, 322);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(143, 49);
+            this.btnOrder.TabIndex = 3;
+            this.btnOrder.Text = "點餐";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(380, 338);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "金額 :";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(441, 322);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(244, 49);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(186, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 31);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(69, 31);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(186, 180);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(69, 31);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(135, 31);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(69, 31);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(135, 65);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(69, 31);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(135, 35);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(69, 31);
+            this.textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(135, 69);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(69, 31);
+            this.textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(135, 102);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(69, 31);
+            this.textBox9.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "個數";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "個數";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "個數";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 397);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("新細明體", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "POSOrder 資工二丙_123456_吳春捲";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkChicken;
+        private System.Windows.Forms.CheckBox chkFish;
+        private System.Windows.Forms.CheckBox chkBPizza;
+        private System.Windows.Forms.CheckBox chkSPizza;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rdbSmall;
+        private System.Windows.Forms.CheckBox chkFries;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton rdbCoffee;
+        private System.Windows.Forms.RadioButton rdbCoke;
+        private System.Windows.Forms.CheckBox chkDrink;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ CS2005701_WindowsProgramming/Exercise1_POS/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSOrder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            int totalAmount = 0;
+
+            if (chkFish.Checked) totalAmount += 59;
+            if (chkChicken.Checked) totalAmount += 69;
+            if (chkSPizza.Checked) totalAmount += 259;
+            if (chkBPizza.Checked) totalAmount += 429;
+
+            if (chkFries.Checked)
+            {
+                // 大薯/小薯
+                if (rdbSmall.Checked)
+                    totalAmount += 25;
+                else
+                    totalAmount += 35;
+            }
+
+            if (chkDrink.Checked)
+            {
+                if (rdbCoke.Checked) totalAmount += 25;
+                else if (rdbCoffee.Checked) totalAmount += 30;
+                else totalAmount += 20;
+            }
+
+            lblOutput.Text = "NT $ " + totalAmount;
+        }
+    }
+}
