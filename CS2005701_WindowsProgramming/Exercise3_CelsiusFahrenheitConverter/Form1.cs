0bdba6e108c508a35f53771bd35c3794146899b5
yxleong <yx0829leong@gmail.com>
2024-10-11 11:50:22 +0800

    updated

---
+++ CS2005701_WindowsProgramming/Exercise3_CelsiusFahrenheitConverter/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelsiusFahrenheitConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            celsiusValue.Enter += celsiusValue_Enter;
+            fahrenheitValue.Enter += fahrenheitValue_Enter;
+        }
+        private void celsiusValue_Enter(object sender, EventArgs e)
+        {
+            fahrenheitValue.Clear();
+        }
+
+        private void fahrenheitValue_Enter(object sender, EventArgs e)
+        {
+            celsiusValue.Clear();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(celsiusValue.Text))
+            {
+                double c = Convert.ToDouble(celsiusValue.Text);
+                double f = c * 9 / 5 + 32;
+                fahrenheitValue.Text = f.ToString("F2");
+            } else if (!string.IsNullOrWhiteSpace(fahrenheitValue.Text))
+            {
+                double f = Convert.ToDouble(fahrenheitValue.Text);
+                double c = (f - 32) / 9 * 5;
+                celsiusValue.Text = c.ToString("F2");
+            } else
+            {
+                MessageBox.Show("Please enter a value in either Celsius or Fahrenheit.");
+            }
+        }
+    }
+}
---
+++ CS2005701_WindowsProgramming/Exercise3_CelsiusFahrenheitConverter/README.md
@@ -0,0 +1,21 @@
+# Temperature Converter
+
+## Overview
+This application allows users to convert temperatures between Celsius and Fahrenheit. Users can input a temperature in either Celsius or Fahrenheit, and the corresponding temperature will be calculated and displayed in the other unit.
+
+## Features
+- Input temperature in Celsius or Fahrenheit.
+- Automatic conversion to the other temperature unit.
+- Clear input fields when switching focus between text boxes.
+- User-friendly interface with error handling for empty inputs.
+
+## How It Works
+1. **Celsius Input:** Enter a temperature in the Celsius text box. When the convert button is clicked, the application calculates the corresponding temperature in Fahrenheit.
+2. **Fahrenheit Input:** Enter a temperature in the Fahrenheit text box. When the convert button is clicked, the application calculates the corresponding temperature in Celsius.
+3. **Clear on Focus:** When a text box gains focus, the other text box is cleared to prevent confusion.
+
+## Demo Result
+![interface](image.png)
+
+![input_c](image-1.png) ![result_f](image-2.png)
+![input_f](image-3.png) ![result_c](image-4.png)
