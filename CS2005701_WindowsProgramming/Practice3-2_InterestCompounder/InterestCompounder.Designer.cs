451f6a385d359e4931325f04ada38e5dc0870326
yxleong <yx0829leong@gmail.com>
2024-10-11 10:24:26 +0800

    ch3-2 uploaded

---
+++ CS2005701_WindowsProgramming/Practice3-2_InterestCompounder/InterestCompounder.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace ch3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.amount = new System.Windows.Forms.Label();
+            this.rate = new System.Windows.Forms.Label();
+            this.cal_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.year = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // amount
+            // 
+            this.amount.AutoSize = true;
+            this.amount.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amount.Location = new System.Drawing.Point(141, 56);
+            this.amount.Name = "amount";
+            this.amount.Size = new System.Drawing.Size(151, 19);
+            this.amount.TabIndex = 0;
+            this.amount.Text = "Enter Principal Amount:";
+            // 
+            // rate
+            // 
+            this.rate.AutoSize = true;
+            this.rate.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rate.Location = new System.Drawing.Point(81, 108);
+            this.rate.Name = "rate";
+            this.rate.Size = new System.Drawing.Size(211, 19);
+            this.rate.TabIndex = 1;
+            this.rate.Text = "Enter Annual Interest Rate (in %):";
+            // 
+            // cal_button
+            // 
+            this.cal_button.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cal_button.Location = new System.Drawing.Point(521, 79);
+            this.cal_button.Name = "cal_button";
+            this.cal_button.Size = new System.Drawing.Size(208, 83);
+            this.cal_button.TabIndex = 4;
+            this.cal_button.Text = "Calculate Compound Amount";
+            this.cal_button.UseVisualStyleBackColor = true;
+            this.cal_button.Click += new System.EventHandler(this.cal_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(316, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(316, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // year
+            // 
+            this.year.AutoSize = true;
+            this.year.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.year.Location = new System.Drawing.Point(63, 158);
+            this.year.Name = "year";
+            this.year.Size = new System.Drawing.Size(229, 19);
+            this.year.TabIndex = 4;
+            this.year.Text = "Enter Investment Duration (in years):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(316, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 26);
+            this.textBox3.TabIndex = 3;
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.result.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(3, 0);
+            this.result.MinimumSize = new System.Drawing.Size(433, 184);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(433, 184);
+            this.result.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.result);
+            this.panel1.Location = new System.Drawing.Point(176, 216);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(439, 186);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.year);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cal_button);
+            this.Controls.Add(this.rate);
+            this.Controls.Add(this.amount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label amount;
+        private System.Windows.Forms.Label rate;
+        private System.Windows.Forms.Button cal_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label year;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ CS2005701_WindowsProgramming/Practice3-2_InterestCompounder/InterestCompounder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cal_button_Click(object sender, EventArgs e)
+        {
+            double amt = Convert.ToDouble(textBox1.Text); // principal investment amount -> P
+            double percent = Convert.ToDouble(textBox2.Text);   // annual interest rate -> r
+            double num = Convert.ToDouble(textBox3.Text);    // number of years -> t
+            double compound = amt;  // compound amount -> A
+            // n = the number of times that interest is compounded per year
+
+            result.Text = "";   // clear previous result for the second time input
+            for (int i = 1; i <= num; i++)
+            {
+                // formula: A = P * ( 1 + r/n ) ^ (n*t)
+                compound *= (1 + percent / 100); // r in decimal
+                result.Text += $"Year{i}: Compound amount is {compound:C}\n";   // compund amount in currency format
+            }
+        }
+    }
+}
