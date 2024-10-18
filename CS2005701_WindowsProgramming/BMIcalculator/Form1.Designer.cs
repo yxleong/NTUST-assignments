03e9ac97b2b11659bfe47a1fca807f7e42b0437a
yxleong <yx0829leong@gmail.com>
2024-10-18 12:10:34 +0800

    updated

--- CS2005701_WindowsProgramming/BMIcalculator/Form1.Designer.cs
+++ CS2005701_WindowsProgramming/BMIcalculator/Form1.Designer.cs
@@ -38,14 +38,14 @@ namespace BMIcalculator
             this.height = new System.Windows.Forms.TextBox();
             this.weight = new System.Windows.Forms.TextBox();
             this.calBtn = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.resultbox = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(66, 47);
+            this.label1.Location = new System.Drawing.Point(105, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 21);
             this.label1.TabIndex = 0;
@@ -55,7 +55,7 @@ namespace BMIcalculator
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(41, 94);
+            this.label2.Location = new System.Drawing.Point(80, 85);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 21);
             this.label2.TabIndex = 1;
@@ -65,26 +65,26 @@ namespace BMIcalculator
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(48, 141);
+            this.label3.Location = new System.Drawing.Point(73, 132);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 21);
+            this.label3.Size = new System.Drawing.Size(76, 21);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Height:";
+            this.label3.Text = "* Height:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(45, 188);
+            this.label4.Location = new System.Drawing.Point(70, 179);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 21);
+            this.label4.Size = new System.Drawing.Size(79, 21);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Weight:";
+            this.label4.Text = "* Weight:";
             // 
             // age
             // 
             this.age.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.age.Location = new System.Drawing.Point(116, 42);
+            this.age.Location = new System.Drawing.Point(155, 33);
             this.age.Name = "age";
             this.age.Size = new System.Drawing.Size(100, 26);
             this.age.TabIndex = 4;
@@ -94,42 +94,45 @@ namespace BMIcalculator
             // 
             this.gender.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gender.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.gender.Location = new System.Drawing.Point(116, 89);
+            this.gender.Location = new System.Drawing.Point(155, 80);
             this.gender.Name = "gender";
             this.gender.Size = new System.Drawing.Size(100, 26);
             this.gender.TabIndex = 5;
             this.gender.Text = "M / F";
             this.gender.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.gender.Enter += new System.EventHandler(this.gender_Enter);
+            this.gender.Leave += new System.EventHandler(this.gender_Leave);
             // 
             // height
             // 
             this.height.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.height.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.height.Location = new System.Drawing.Point(116, 136);
+            this.height.Location = new System.Drawing.Point(155, 127);
             this.height.Name = "height";
             this.height.Size = new System.Drawing.Size(100, 26);
             this.height.TabIndex = 6;
             this.height.Text = "m";
             this.height.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.height.Enter += new System.EventHandler(this.height_Enter);
+            this.height.Leave += new System.EventHandler(this.height_Leave);
             // 
             // weight
             // 
             this.weight.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.weight.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.weight.Location = new System.Drawing.Point(116, 183);
+            this.weight.Location = new System.Drawing.Point(155, 174);
             this.weight.Name = "weight";
             this.weight.Size = new System.Drawing.Size(100, 26);
             this.weight.TabIndex = 7;
             this.weight.Text = "kg";
             this.weight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.weight.Enter += new System.EventHandler(this.weight_Enter);
+            this.weight.Leave += new System.EventHandler(this.weight_Leave);
             // 
             // calBtn
             // 
             this.calBtn.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.calBtn.Location = new System.Drawing.Point(243, 23);
+            this.calBtn.Location = new System.Drawing.Point(325, 80);
             this.calBtn.Name = "calBtn";
             this.calBtn.Size = new System.Drawing.Size(100, 73);
             this.calBtn.TabIndex = 8;
@@ -137,21 +140,23 @@ namespace BMIcalculator
             this.calBtn.UseVisualStyleBackColor = true;
             this.calBtn.Click += new System.EventHandler(this.calBtn_Click);
             // 
-            // label5
+            // resultbox
             // 
-            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(233, 106);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(122, 115);
-            this.label5.TabIndex = 9;
+            this.resultbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultbox.Font = new System.Drawing.Font("Times New Roman", 9F);
+            this.resultbox.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.resultbox.Location = new System.Drawing.Point(31, 217);
+            this.resultbox.Name = "resultbox";
+            this.resultbox.Size = new System.Drawing.Size(447, 122);
+            this.resultbox.TabIndex = 9;
+            this.resultbox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 248);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(511, 368);
+            this.Controls.Add(this.resultbox);
             this.Controls.Add(this.calBtn);
             this.Controls.Add(this.weight);
             this.Controls.Add(this.height);
@@ -179,7 +184,7 @@ namespace BMIcalculator
         private System.Windows.Forms.TextBox height;
         private System.Windows.Forms.TextBox weight;
         private System.Windows.Forms.Button calBtn;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label resultbox;
     }
 }
 
--- CS2005701_WindowsProgramming/BMIcalculator/Form1.cs
+++ CS2005701_WindowsProgramming/BMIcalculator/Form1.cs
@@ -12,51 +12,129 @@ namespace BMIcalculator
 {
     public partial class Form1 : Form
     {
+        private void InitResultBox(Label resultbox)
+        {
+            resultbox.Text = "[ BMI Categories ]\n" +
+                "Underweight: BMI < 18.5\n" +
+                "Normal Range: 18.5 ≤ BMI < 24\n" +
+                "Overweight: 24 ≤ BMI < 27\n" +
+                "Mild Obesity: 27 ≤ BMI < 30\n" +
+                "Moderate Obesity: 30 ≤ BMI < 35\n" +
+                "Severe Obesity: BMI ≥ 35"; ;
+            resultbox.ForeColor = SystemColors.ActiveCaption;
+            resultbox.Font = new Font(resultbox.Font.FontFamily, 9);
+        }
+
+        private void InitTextBox(TextBox inputbox, string text)
+        {
+            if (inputbox.Text == "")
+            {
+                inputbox.Text = text;
+                inputbox.ForeColor = SystemColors.InactiveCaption;
+            }
+        }
+
+        private void SetTextBox(TextBox inputbox)
+        {
+            inputbox.Clear();
+            inputbox.ForeColor = Color.Black;
+        }
+
+        private void SetWarningMsg(String text)
+        {
+            MessageBox.Show(text, "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
         public Form1()
         {
             InitializeComponent();
+            InitResultBox(resultbox);
         }
+
         private void gender_Enter(object sender, EventArgs e)
         {
-            gender.Clear();
-            gender.ForeColor = Color.Black;
+            SetTextBox(gender);
         }
 
         private void height_Enter(object sender, EventArgs e)
         {
-            height.Clear();
-            height.ForeColor = Color.Black;
+            SetTextBox(height);
+            InitResultBox(resultbox);
         }
 
         private void weight_Enter(object sender, EventArgs e)
         {
-            weight.Clear();
-            weight.ForeColor = Color.Black;
+            SetTextBox(weight);
+            InitResultBox(resultbox);
+        }
+
+        private void gender_Leave(object sender, EventArgs e)
+        {
+            InitTextBox(gender, "M / F");
+        }
+
+        private void height_Leave(object sender, EventArgs e)
+        {
+            InitTextBox(height, "m");
+        }
+
+        private void weight_Leave(object sender, EventArgs e)
+        {
+            InitTextBox(weight, "kg");
         }
 
         private void calBtn_Click(object sender, EventArgs e)
         {
-            double w = Convert.ToDouble(weight.Text);
-            double h = Convert.ToDouble(height.Text);
-            double bmi = w / h;
+            double w = 0, h = 0;
             string result = "";
 
-            if(bmi < 18.5)
+            if (string.IsNullOrWhiteSpace(weight.Text) && string.IsNullOrWhiteSpace(height.Text))
             {
-                result = "Underweight";
+                SetWarningMsg("Please enter both your weight and height to calculate your BMI.");
+            }else if (string.IsNullOrWhiteSpace(weight.Text))
+            {
+                SetWarningMsg("Please enter your weight to calculate your BMI.");
             }
-            else if (bmi < 24.9)
+            else if (string.IsNullOrWhiteSpace(height.Text))
             {
-                result = "Normal weight";
-            }
-            else if (bmi < 29.9)
-            {
-                result = "Overweight";
+                SetWarningMsg("Please enter your height to calculate your BMI.");
             }
             else
             {
-                result = "Obesity";
-            }
+                if (!double.TryParse(weight.Text, out w) || !double.TryParse(height.Text, out h))
+                {
+                    MessageBox.Show("Please enter valid numerical values for weight and height.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    w = Convert.ToDouble(weight.Text);
+                    h = Convert.ToDouble(height.Text);
+
+                    double bmi = w / h / h;
+
+                    if (bmi < 18.5)
+                    {
+                        result = bmi.ToString("F2") + " = Underweight";
+                    }
+                    else if (bmi < 24.9)
+                    {
+                        result = bmi.ToString("F2") + " = Normal weight";
+                    }
+                    else if (bmi < 29.9)
+                    {
+                        result = bmi.ToString("F2") + " = Overweight";
+                    }
+                    else
+                    {
+                        result = bmi.ToString("F2") + " = Obesity";
+                    }
+
+                    resultbox.Text = result;
+                    resultbox.ForeColor = Color.Black;
+                    resultbox.Font = new Font(resultbox.Font.FontFamily, 33);
+                }
+            }        
+           
         }
     }
 }
